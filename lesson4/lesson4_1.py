f0bc8f7e3c69cc03e1ad3b581834d262684bc0b9
KrutkoAndrii <kroo@ukr.net>
2019-06-23 23:43:51 +0300

    try 21/06/2019 lesson4

---
+++ lesson4/lesson4_1.py
@@ -0,0 +1,80 @@
+
+'''
+    Пользователь задает случайное число n. Сгенерировать
+    список этой длины и заполнить его 0 и 1 случайным образом.
+    Найти самую длинную цепочку из подряд идущих 0 или 1.
+    Вывести эту длину. Для какого максимального значения n,
+    ваш алгоритм будет работать меньше чем 1 секунда?
+    '''
+import random
+import datetime
+
+
+def check_input_digit(string):
+    user_digit = input(string)
+    while not user_digit.isdigit():
+        user_digit = input("Wrong data. Try again: ")
+    return int(user_digit)
+
+
+def generate_random_of_array(length_of_array):
+    print("Generated array...")
+    random_array = [random.randint(0, 1) for i in range(length_of_array)]
+    # print(random_array)
+    return random_array
+
+
+# Time of searching 0.97 sec for array 20 000 000 items
+# - CPU i3 6100  8 Gb , Windows 10
+def find_the_longest_sequence(array_of_sequence):
+    print('Searching...')
+    time_start = datetime.datetime.now()
+    item = 0
+    count_of_item = 0
+    max_item = 0
+    type_of_max_item = 0
+    for item_of_sequence in array_of_sequence:
+        if item_of_sequence == item:
+            count_of_item += 1
+        elif max_item < count_of_item:
+            count_of_item += 1
+            max_item = count_of_item
+            type_of_max_item = item
+            item = item_of_sequence
+            count_of_item = 0
+        else:
+            item = item_of_sequence
+            count_of_item = 0
+    time_finish = datetime.datetime.now()
+    elapsed_time = time_finish - time_start
+    print("Time of searching is: " + str(elapsed_time))
+    print("Max length is: " + str(max_item) + ", and its: "
+          + str(type_of_max_item))
+
+
+# Time of searching 5.35 sec for array 20 000 000 items
+# - CPU i3 6100  8 Gb , Windows 10
+# but if we have prepared string time decrease to 0.06 sec
+def find_the_longest_sequence_alternative(array_of_sequence):
+    print('Searching alternative way...')
+    time_start = datetime.datetime.now()
+    string_of_array = ''.join(str(val) for val in array_of_sequence)
+    for type_of_item in ['0', '1']:
+        result = 0
+        string_of_finding = ''
+        while result != -1:
+            string_of_finding += type_of_item
+            result = string_of_array.find(string_of_finding)
+        print('Max length of ' + type_of_item + ' is: '
+              + str(len(string_of_finding)-1))
+    time_finish = datetime.datetime.now()
+    elapsed_time = time_finish - time_start
+    print("Time of searching is: " + str(elapsed_time))
+
+
+if __name__ == "__main__":
+    length = check_input_digit("Input length of list: ")
+    print("Wait...")
+    array_by_item = generate_random_of_array(length)
+    find_the_longest_sequence(array_by_item)
+    find_the_longest_sequence_alternative(array_by_item)
---
+++ lesson4/lesson4_3.py
@@ -0,0 +1,63 @@
+'''
+ Без использования методов строк, напишите реализацию таких методов строк:
+ replace, split, find. Напишите функцию remove по индексу и по подстроке.
+  '''
+
+
+def replace(string, substring, substring_new, count_of_replace=-1):
+    string_new = ''
+    counter = 0
+    string_len = len(substring)
+    string_index = 0
+    while string_index < len(string):
+        if string[string_index:string_index + string_len] == substring:
+            string_new += substring_new
+            string_index += string_len
+            counter += 1
+            if counter == count_of_replace:
+                return string_new + string[string_index:]
+        else:
+            string_new += string[string_index]
+            string_index += 1
+    return string_new
+
+
+def split(string, separator, count_of_split=-1):
+    list_result = []
+    string_new = ''
+    string_len = len(str(separator))
+    string_index = 0
+    counter = 0
+    while string_index < len(string):
+        if string[string_index:string_index + string_len] == separator:
+            list_result.append(string_new)
+            string_new = ''
+            string_index += string_len
+            counter += 1
+            if counter == count_of_split:
+                list_result.append(string[string_index:])
+                return list_result
+        else:
+            string_new += string[string_index]
+            string_index += 1
+    list_result.append(string_new)
+    return list_result
+
+
+def find(string, substring, start=0, end=-1):
+    if end == -1:
+        end = len(string)
+    string_index = 0
+    string_len = len(str(substring))
+    while string_index < len(string[start:end]):
+        if string[string_index+start:string_index
+                                    + start + string_len] == substring:
+            return string_index+start
+        string_index += 1
+    return -1
+
+
+if __name__ == "__main__":
+    print(replace('This is the island of istanbul', 'is', 'was', 2))
+    print(split('This is the island of istanbul', ' ', 2))
+    print(find('This is the island of istanbul', 'the', 7, 17))
