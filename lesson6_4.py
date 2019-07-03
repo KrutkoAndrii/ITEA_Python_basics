abc5849f01cc90881913387afd5e454d046cf08b
KrutkoAndrii <kroo@ukr.net>
2019-07-03 20:34:58 +0300

    03/07/2019 final

--- lesson6_4.py
+++ lesson6_4.py
@@ -4,15 +4,29 @@
     кожен елемент складає з себе середнє арифметичне кожного рядку файла
     test3_result.txt. Тобто 100 елементний список.
 '''
+
 import pickle
 
+
+def sum_of_array(function_import,arrays_of_digits):
+    print(eval(function_import))
+
+
+def make_result():
+    array_of_averages = []
+    with open('test3_result.txt', 'r') as file_data:
+        for sting_from_file in file_data:
+            array_from_strings = [int(s) for s in
+                                  sting_from_file[:-1].rsplit(' ')]
+            array_of_averages.append(sum(array_from_strings) /
+                                     len(array_from_strings))
+    file_data.close()
+    return array_of_averages
 
 
 if __name__ == "__main__":
     with open('test2.txt', 'rb') as file_def:
         function_from_file = pickle.load(file_def)
-        print(function_from_file)
-        arrays_of_digits = [2, 3, 4]
-        print(eval(function_from_file))
-       # print(function_from_file([2,3,4]))
-        #exec(function_from_file)
+    file_def.close()
+    array_of_digits = make_result()
+    sum_of_array(function_from_file, array_of_digits)
